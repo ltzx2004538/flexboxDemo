292ad07ed2e477d026f0e3492e0badc19e533620
yyr19921203 <123775172@qq.com>
2020-09-10 01:28:15 +1000

    build flexbox

--- flexbox/src/components/FlexDemo.js
+++ flexbox/src/components/FlexDemo.js
@@ -2,21 +2,55 @@ import React from 'react';
 import "./FlexDemo.scss"
 
 
+class FlexDemo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            active: true,
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        const newState = !this.state.active
+        this.setState({
+            active: newState,
+        })
+    }
 
 
-const FlexDemo=()=>{
 
-
-    return(
-        <div className="container">
-            <div className="leftDiv">
-                <span>left</span>
+    render() {
+        const active = this.state.active;
+        return (
+            <React.Fragment>
+            <div className="container">
+                <div className="leftDiv">
+                    <span>left</span>
+                </div>
+                {active ?
+                    <div className="rightDiv-short">
+                        <span>right</span>
+                    </div>
+                    :
+                    <div className="rightDiv-long">
+                        <span>right</span>
+                    </div>
+                }
             </div>
-            <div className="rightDiv">
-                <span>right</span>
-            </div>
-        </div>
-    );
+             <button onClick={(event)=>{
+                 event.preventDefault();
+                 this.handleClick();
+             }}>change length</button>
+             </React.Fragment>
+        );
+    }
+
+
+
+
+
 }
 
 export default FlexDemo;
