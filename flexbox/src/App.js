e03310087758c4919bcaaf9bc73470a41cb47694
yyr19921203 <123775172@qq.com>
2020-09-09 22:33:03 +1000

    fix css of sendbar

---
+++ flexbox/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import FlexDemo from'./components/FlexDemo';
+
+function App() {
+  return (
+    <div className="App">
+     <FlexDemo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ flexbox/src/components/FlexDemo.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import "./FlexDemo.scss"
+
+
+
+
+const FlexDemo=()=>{
+
+
+    return(
+        <div className="container">
+            <div className="leftDiv">
+                <span>left</span>
+            </div>
+            <div className="rightDiv">
+                <span>right</span>
+            </div>
+        </div>
+    );
+}
+
+export default FlexDemo;
